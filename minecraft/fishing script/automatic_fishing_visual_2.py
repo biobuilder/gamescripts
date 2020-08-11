b592f740eca0e73cd3e2df7bc8b0cec87f169d13
biobuilder <damon.printz@gmail.com>
2020-08-11 12:06:39 -0600

    updated documentation
    added lots of code comments for new users

--- minecraft/fishing script/automatic_fishing_visual_2.py
+++ minecraft/fishing script/automatic_fishing_visual_2.py
@@ -2,23 +2,91 @@
 """
 Created on Wed Jul 15 21:40:31 2020
 
-@author: Damon
+@author: Biobuilder aka Incrediguy
 
-Looks at the screen for a bobber
-If the bobber is not found, the
-script triggers the right mouse button
-the button has a cooldown
-The script switches poles after a certain number
-of uses
+ - - - What This Script Does - - -
+This is an automatic fishing script for minecraft which uses screenshots, keyboard and mouse functions.
+The program plays minecraft exactly the same way a human would, except it never gets tired.
+This program does not modify minecraft game files in any way.
+The program looks at the screen for a bobber.
+If the bobber is not found or is underwater, the script clicks the right mouse button.
+The button has a cooldown.
+The script switches poles after a certain number of uses.
+The pole selection count is incremented once after the number of casts are reached.
+
+
+ - - - General Use Tips - - -
+Fill your quick slots with fishing poles for maximum effect in Survival mode.
+I would recommend using around 200 casts to completely consume an unenchanted fishing pole.
+
+Look at "focus.png" to see where the script is looking on your screen.
+Make sure the bobber is visible somewhere in this picture when casting the line.
+
+Make sure you use the escape character (') to terminate the script, otherwise it will keep running.
+
+                                       
+ - - - Construction Requirements - - -       
+Make sure the script cannot directly see any torches or other red light sources.                               
+You need to build a small structure containing water (2x5x7 only water blocks).
+You should build a safe fishing platform at least 2 blocks above this water.
+You should throw the bobber at a solid block to make it fall exactly in the center.
+You should build hoppers underwater to collect the items.  
+Put jack-o-lanterns underwater to brighten up the bobber for night fishing.
+Surround the place with torches for good lighting but don't let the script see these torches directly.
+
+ - - - Software Requirements - - - 
+How to make this script work:
+You need to install Python
+ - I would recommend installing an IDE like Spyder which makes it easy to run this script
+ - otherwise you are going to have to use the command line to run the script
+You need to install the mouse library (pip install mouse)
+You need to install the keyboard library (type "pip install keyboard" in a terminal)
+You need to install the pyautogui library (type "pip install pyautogui" in a terminal)
+You need to install PIL (this may be included by default)
+The time library is likely to be included by default
 
 """
 
-import mouse
-import keyboard
-import pyautogui
-import time
 
-from PIL import Image
+
+
+
+# - - - - - - - - - - - Modify Here - - - - - - - - - -
+
+POLE_SLOT_START = 6     # starting fishing pole slot (must be >= 1 and <= 9)
+POLE_SLOT_END = 6       # ending fishing pole slot (must be >= 1 and <= 9)
+POLE_MAX_USES = 10000   # how many uses (casts) a pole has before it switches
+
+# you can change where on your screen the script looks for a bobber
+# the program has no idea where the minecraft window is, so you need to put
+# your game window wherever is convenient and then adjust the coordinates
+# until you find your bobber
+# look at the "focus.png" screenshot in your script folder to see where 
+# the script is looking
+PICTURE_X = 375         # screenshot top left corner x screen coordinate
+PICTURE_Y = 600         # screenshot top left corner y screen coordinate
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+
+
+
+
+
+
+
+
+# - - - - - - - - Advanced Users Modify Here - - - - - - 
+
+# picture constants - larger pictures are more work for the script to process
+PICTURE_W = 200     # width of the screenshot
+PICTURE_H = 200     # height of the screenshot
+
+# red bobber color threshold
+RED_THRESHOLD = (130, 100, 100)
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+
 
 # constants
 FISHING_CHAR = ';'
@@ -27,10 +95,6 @@ SCREENSHOT_CHAR = ']'
 SCREENSHOT_CODE = 27 # character = ']'
 ESCAPE_CHAR = '\''
 ESCAPE_CODE = 40 # character = '\''
-
-POLE_SLOT_START = 6 # starting fishing pole slot (must be greater than 1 and less than 9)
-POLE_SLOT_END = 6 # ending fishing pole slot (must be greater than 1 and less than 9)
-POLE_MAX_USES = 10000  # how many uses (casts) a pole has before it switches
 
 # program states
 SCREENSHOT_STATE = 1
@@ -42,10 +106,6 @@ MOUSE_BUTTON = 'right' # mouse button to push
 MOUSE_COOLDOWN = 3 # number of seconds to wait between successive clicks
 CHECK_COOLDOWN = 0.5 # number of seconds to wait between checking the screen
 
-# red bobber color threshold
-RED_THRESHOLD = (130, 100, 100)
-
-
 # global variables
 take_screenshot = False  # take a screenshot flag
 esc_triggered = False    # escape from the program flag
@@ -54,6 +114,13 @@ state = SCREENSHOT_STATE # default program state
 
 pole_slot = POLE_SLOT_START # currently selected fishing pole
 pole_uses = 0   # keeps track of how many times the selected pole has been used
+
+# import libraries
+import mouse
+import keyboard
+import pyautogui
+import time
+from PIL import Image
 
 # keyboard event handler
 def handle_keyboard_event(e):
@@ -71,7 +138,7 @@ def handle_keyboard_event(e):
 
 # take a screenshot of the minecraft screen
 def takeScreenshot():
-    myScreenshot = pyautogui.screenshot(region=(375,600,200,200))
+    myScreenshot = pyautogui.screenshot(region=(PICTURE_X,PICTURE_Y,PICTURE_W,PICTURE_H))
     myScreenshot.save(r'focus.png')
 
 # create keyboard handle
@@ -89,8 +156,7 @@ print('Exit = ' + ESCAPE_CHAR)
 # take the first screenshot
 takeScreenshot()
 
-# search for a pixel that is redder than the
-# Red threshold
+# search for a pixel that is redder than the Red threshold
 
 # enter main loop
 while( esc_triggered == False ):
